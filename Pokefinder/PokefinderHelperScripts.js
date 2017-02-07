49d7cb85935aee179b87f0d1348b8dac8c72c68c
Henry Weeks <henrydweeks2511@gmail.com>
2017-02-07 09:57:20 -0600

    Fine tuning UX

--- Pokefinder/PokefinderHelperScripts.js
+++ Pokefinder/PokefinderHelperScripts.js
@@ -2,31 +2,19 @@
 // of Pokefinder
 $(document).ready(function () {
     $("#cityCheck").click(function () {
-        $("#pokemonIDCheck").prop('checked', false);
-        $("#pokemonNameCheck").prop('checked', false);
-    });
-    $("#pokemonIDCheck").click(function () {
-        $("#cityCheck").prop('checked', false);
         $("#pokemonNameCheck").prop('checked', false);
     });
     $("#pokemonNameCheck").click(function () {
-        $("#pokemonIDCheck").prop('checked', false);
         $("#cityCheck").prop('checked', false);
     });
     $("#searchButton").click(function () {
         if (document.getElementById('cityCheck').checked) {
+
             $("#cityResults").animate({ height: "show" }, "slow");
-            $("#iDResults").animate({ height: "hide" }, "slow");
-            $("#nameResults").animate({ height: "hide" }, "slow");
-        }
-        if (document.getElementById('pokemonIDCheck').checked) {
-            $("#iDResults").animate({ height: "show" }, "slow");
-            $("#cityResults").animate({ height: "hide" }, "slow");
             $("#nameResults").animate({ height: "hide" }, "slow");
         }
         if (document.getElementById('pokemonNameCheck').checked) {
             $("#nameResults").animate({ height: "show" }, "slow");
-            $("#iDResults").animate({ height: "hide" }, "slow");
             $("#cityResults").animate({ height: "hide" }, "slow");
         }
         });
